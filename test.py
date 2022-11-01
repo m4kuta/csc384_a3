9e0f0dae2549007f3a167abda1723fef1335fcb3
haolin <haolinf@gmail.com>
2022-11-01 18:34:26 -0400

    Notes and test file

---
+++ test.py
@@ -0,0 +1,20 @@
+﻿from battle import *
+
+# inputPath = "battle_validate/input_easy1.txt"
+inputPath = "battle_validate/input_easy2.txt"
+
+print('### readBoard ###')
+board = readBoard(inputPath)
+board.print()
+
+print('### defineVariables ###')
+board.defineVariables()
+board.print()
+
+print('### preprocess ###')
+board.preprocess()
+board.print()
+
+print('### solveCSP ###')
+# solveCSP(board)
+board.print()
